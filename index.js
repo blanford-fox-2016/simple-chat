085336d4124d1f252a1214899ed5e28541327937
Septian A Tama <sfirst.tama@gmail.com>
2016-11-10 10:35:20 +0700

    add routes to send index html

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
