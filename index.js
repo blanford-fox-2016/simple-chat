8ec186ca4066037a000f4876dcd125bb68d18a14
Septian A Tama <sfirst.tama@gmail.com>
2016-11-10 10:41:21 +0700

    add socket.io connection on both client and server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+'use strict'
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3001, function(){
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});;
+
+http.listen(3001, () => {
   console.log('listening on *:3001');
 });
